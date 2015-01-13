e48efe9822a627d151338cbacc599d4f8a7d3b14
Aljosa Vodopija <aljosa.vodopija@gmail.com>
2015-01-13 21:52:39 +0100

    Dodan prostor celih števil

---
+++ Integers.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE TypeSynonymInstances, MultiParamTypeClasses, FlexibleInstances #-}
+
+module Integers where
+
+import Staged
+import Space
+import Searchable
+
+-- | Integers (Z) with discrete topology form a 
+-- | LinearOrder, Discrete, Hausdorff, Overt space
+-- | Compact subspaces are finite sets 
+ 
+-- | Linear order on integers
+instance LinearOrder Integer where
+	less x y = return $ x < y
+
+-- | The Discrete property
+instance Discrete Integer where
+	equal x y = return $ x == y
+
+-- | The Hausdorff property
+instance Hausdorff Integer where
+	apart x y = return $ (x < y || x > y)
+
+-- | Subset of the integers
+type SubSet = [Integer]
+
+-- | Compactness of the finite subsets
+instance Compact SubSet Integer where
+	forall s p = return $ forall' (set s) (force . p)
+	
+-- | Overtness of any subset
+instance Overt SubSet Integer where
+	exists s p = return $ exists' (set s) (force . p)
+	
